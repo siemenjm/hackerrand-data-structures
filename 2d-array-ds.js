d2e36b7c20dab59d3ea9714690aba502ce212f6e
Jared Siemen <siemenjm@gmail.com>
2022-12-31 11:07:24 -0500

    Initial commit

---
+++ 2d-array-ds.js
@@ -0,0 +1,10 @@
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function hourglassSum(arr) {
+  // Write your code here
+}
