f8d839820ab43c7ccad2ddf9634f4af39e8265fa
Jared Siemen <siemenjm@gmail.com>
2022-12-31 13:23:05 -0500

    Finish dynamic array problem

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,12 +1,3 @@
-/*
- * Complete the 'dynamicArray' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER n
- *  2. 2D_INTEGER_ARRAY queries
- */
-
 // Declare a 2-dimensional array, , of  empty arrays. All arrays are zero indexed.
 // Declare an integer, , and initialize it to .
 
@@ -54,6 +45,48 @@
 // 7
 // 3
 
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
 function dynamicArray(n, queries) {
-  // Write your code here
+  let answersArr = [];
+
+  // Construct 2d array of n arrays
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push([]);
+  }
+
+  let lastAnswer = 0;
+
+  // Loop through all queries
+  queries.forEach((query) => {
+    let idx = (query[1] ^ lastAnswer) % n;
+
+    if (query[0] === 1) {
+      arr[idx].push(query[2]);
+    } else if (query[0] === 2) {
+      lastAnswer = arr[idx][query[2] % arr[idx].length];
+      answersArr.push(lastAnswer);
+    }
+  });
+
+  return answersArr;
 }
+
+const testN = 2;
+const testQueries = [
+  [1, 0, 5],
+  [1, 1, 7],
+  [1, 0, 3],
+  [2, 1, 0],
+  [2, 1, 1],
+];
+
+console.log(dynamicArray(testN, testQueries));
