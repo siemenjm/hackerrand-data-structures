d1c5731d1f6c23c708dbe8352457ddee29d40bd4
Jared Siemen <siemenjm@gmail.com>
2022-12-31 12:56:10 -0500

    Find the greatest hourglass sum in a 2d array

--- 2d-array-ds.js
+++ 2d-array-ds.js
@@ -5,6 +5,53 @@
  * The function accepts 2D_INTEGER_ARRAY arr as parameter.
  */
 
+/**
+ * create var to store max hourglass sum in
+ * loop through all the hourglasses
+ * loop through each hourglass to find sum
+ * if hourglass sum is higher than previous hourglass sum, update
+ */
+
 function hourglassSum(arr) {
-  // Write your code here
+  let maxSum = undefined;
+
+  for (let i = 1; i <= 4; i++) {
+    for (let j = 1; j <= 4; j++) {
+      let topRowSum = 0;
+      let bottomRowSum = 0;
+      for (let col = j - 1; col <= j + 1; col++) {
+        topRowSum += arr[i - 1][col];
+        bottomRowSum += arr[i + 1][col];
+      }
+
+      const thisSum = topRowSum + bottomRowSum + arr[i][j];
+
+      if (thisSum > maxSum || maxSum === undefined) {
+        maxSum = thisSum;
+      }
+    }
+  }
+
+  return maxSum;
 }
+
+const testArr1 = [
+  [1, 1, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0],
+  [0, 0, 2, 4, 4, 0],
+  [0, 0, 0, 2, 0, 0],
+  [0, 0, 1, 2, 4, 0],
+]; // 16 hourglasses total in 6x6 array (4x4 total allowed positions)
+
+const testArr2 = [
+  [-9, -9, -9, 1, 1, 1],
+  [0, -9, 0, 4, 3, 2],
+  [-9, -9, -9, 1, 2, 3],
+  [0, 0, 8, 6, 6, 0],
+  [0, 0, 0, -2, 0, 0],
+  [0, 0, 1, 2, 4, 0],
+];
+
+console.log(hourglassSum(testArr1));
+console.log(hourglassSum(testArr2));
