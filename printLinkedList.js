b9fff4c51a0818dc90d03cc7d7d887ac73e4a07b
Jared Siemen <siemenjm@gmail.com>
2022-12-31 14:39:21 -0500

    Initial commit

---
+++ printLinkedList.js
@@ -0,0 +1,12 @@
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function printLinkedList(head) {}
