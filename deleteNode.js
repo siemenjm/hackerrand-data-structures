0bd6fd956911cdfa8bac4b407b407897fd7ed29e
Jared Siemen <siemenjm@gmail.com>
2023-01-01 12:11:12 -0500

    Delete a node from a linked list

--- deleteNode.js
+++ deleteNode.js
@@ -17,6 +17,37 @@
  *
  */
 
-function deleteNode(llist, position) {
-  // Write your code here
+class SinglyLinkedListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
+
+function deleteNode(llist, position) {
+  if (position === 0) {
+    return llist.next;
+  }
+
+  let currentNode = llist;
+  let index = 0;
+  while (index < position - 1) {
+    currentNode = currentNode.next;
+    index++;
+  }
+
+  currentNode.next = currentNode.next.next;
+
+  return llist;
+}
+
+let llist = new SinglyLinkedListNode(2);
+llist.next = new SinglyLinkedListNode(5);
+llist = deleteNode(llist, 0);
+console.log(llist);
+
+llist.next = new SinglyLinkedListNode(10);
+llist.next.next = new SinglyLinkedListNode(15);
+llist.next.next.next = new SinglyLinkedListNode(20);
+llist = deleteNode(llist, 1);
+console.log(llist);
