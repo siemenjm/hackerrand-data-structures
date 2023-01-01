89ddae2b54c7edd373aec9a8577c58ccf1d6c8da
Jared Siemen <siemenjm@gmail.com>
2023-01-01 09:49:50 -0500

    Initial commit

---
+++ insertNodeAtHead.js
@@ -0,0 +1,20 @@
+// Complete the insertNodeAtHead function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+class SinglyLinkedListNode {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function insertNodeAtHead(head, data) {}
