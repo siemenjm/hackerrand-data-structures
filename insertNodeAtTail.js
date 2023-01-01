cb0e16f622045f3505c58498ab92c94b37c491ad
Jared Siemen <siemenjm@gmail.com>
2023-01-01 09:26:12 -0500

    Insert node at tail of linked list

--- insertNodeAtTail.js
+++ insertNodeAtTail.js
@@ -8,4 +8,39 @@
  *
  */
 
-function insertNodeAtTail(head, data) {}
+class SinglyLinkedListNode {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function insertNodeAtTail(head, data) {
+  const newNode = new SinglyLinkedListNode(data);
+  let currentNode = head;
+
+  if (!currentNode) {
+    return newNode;
+  }
+
+  while (currentNode.next) {
+    currentNode = currentNode.next;
+  }
+
+  currentNode.next = newNode;
+  return head;
+}
+
+let linkedListHead = null;
+
+linkedListHead = insertNodeAtTail(linkedListHead, 5);
+console.log(linkedListHead);
+
+linkedListHead = insertNodeAtTail(linkedListHead, 10);
+console.log(linkedListHead);
+
+linkedListHead = insertNodeAtTail(linkedListHead, 15);
+console.log(linkedListHead);
+
+linkedListHead = insertNodeAtTail(linkedListHead, 20);
+console.log(linkedListHead);
