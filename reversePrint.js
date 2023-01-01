e0d532f55f5bdd0e8758ae3a8cc4ca1bd34f66f3
Jared Siemen <siemenjm@gmail.com>
2023-01-01 12:16:38 -0500

    Initial commit

---
+++ reversePrint.js
@@ -0,0 +1,19 @@
+/*
+ * Complete the 'reversePrint' function below.
+ *
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function reversePrint(llist) {
+  // Write your code here
+}
