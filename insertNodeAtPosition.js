74447a6f1ed92162bbb47f1bdfa757594edae773
Jared Siemen <siemenjm@gmail.com>
2023-01-01 10:28:30 -0500

    Initial commit

---
+++ insertNodeAtPosition.js
@@ -0,0 +1,30 @@
+/*
+ * Complete the 'insertNodeAtPosition' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER data
+ *  3. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+class SinglyLinkedListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+function insertNodeAtPosition(llist, data, position) {
+  // Write your code here
+}
