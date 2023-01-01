25f55f6d4377dd8f0b6d2643a81871e1c0fc6429
Jared Siemen <siemenjm@gmail.com>
2023-01-01 11:57:14 -0500

    Initiial commit

---
+++ deleteNode.js
@@ -0,0 +1,22 @@
+/*
+ * Complete the 'deleteNode' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function deleteNode(llist, position) {
+  // Write your code here
+}
