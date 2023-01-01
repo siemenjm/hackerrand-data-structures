036ddecbffbe5aff408ebeddd8db2da0f577188d
Jared Siemen <siemenjm@gmail.com>
2022-12-31 22:15:26 -0500

    Print the data in all the nodes of a singly linked list

--- printLinkedList.js
+++ printLinkedList.js
@@ -9,4 +9,28 @@
  * }
  *
  */
-function printLinkedList(head) {}
+
+class SinglyLinkedListNode {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function printLinkedList(head) {
+  console.log(head.data);
+
+  while (head.next) {
+    head = head.next;
+    console.log(head.data);
+  }
+}
+
+const node1 = new SinglyLinkedListNode(5);
+const node2 = new SinglyLinkedListNode(10);
+const node3 = new SinglyLinkedListNode(15);
+
+node1.next = node2;
+node2.next = node3;
+
+printLinkedList(node1);
