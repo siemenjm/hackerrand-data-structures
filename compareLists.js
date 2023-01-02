41480c2625ad59e305ee9c58ce22f203a7c2cc20
Jared Siemen <siemenjm@gmail.com>
2023-01-02 10:50:40 -0500

    Initial commit

---
+++ compareLists.js
@@ -0,0 +1,20 @@
+// Complete the CompareLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+class SinglyLinkedListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+function CompareLists(llist1, llist2) {}
