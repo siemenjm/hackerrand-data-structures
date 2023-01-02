be2e732fad98e672ed751d7bfba27fa9b58d4013
Jared Siemen <siemenjm@gmail.com>
2023-01-02 11:23:47 -0500

    Initial commit

---
+++ mergeLists.js
@@ -0,0 +1,20 @@
+// Complete the mergeLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+class SinglyLinkedListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+function mergeLists(head1, head2) {}
