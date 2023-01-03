210ae3361c6d4766196e0cd452192ccc3940f1dd
Jared Siemen <siemenjm@gmail.com>
2023-01-02 20:29:24 -0500

    Merge two sorted linked lists into one sorted linked list

--- mergeLists.js
+++ mergeLists.js
@@ -17,4 +17,38 @@ class SinglyLinkedListNode {
   }
 }
 
-function mergeLists(head1, head2) {}
+function mergeLists(head1, head2) {
+  let mergedList = new SinglyLinkedListNode();
+  let currentNode = mergedList;
+
+  while (head1 != null || head2 != null) {
+    if (head1 == null) {
+      currentNode.data = head2.data;
+      head2 = head2.next;
+    } else if (head2 == null || head1.data <= head2.data) {
+      currentNode.data = head1.data;
+      head1 = head1.next;
+    } else {
+      currentNode.data = head2.data;
+      head2 = head2.next;
+    }
+
+    if (head1 == null && head2 == null) {
+      break;
+    }
+
+    currentNode.next = new SinglyLinkedListNode();
+    currentNode = currentNode.next;
+  }
+
+  return mergedList;
+}
+
+let llist1 = new SinglyLinkedListNode(1);
+llist1.next = new SinglyLinkedListNode(3);
+llist1.next.next = new SinglyLinkedListNode(7);
+
+let llist2 = new SinglyLinkedListNode(1);
+llist2.next = new SinglyLinkedListNode(2);
+
+console.log(mergeLists(llist1, llist2));
