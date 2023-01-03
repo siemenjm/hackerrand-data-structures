b8693c8dbcfee0bc559c9b1dd6ddb042e4b691b3
Jared Siemen <siemenjm@gmail.com>
2023-01-02 20:30:55 -0500

    Initial commit

---
+++ getNode.js
@@ -0,0 +1,29 @@
+/*
+ * Complete the 'getNode' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER positionFromTail
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+class SinglyLinkedListNode {
+  constructor(data = null, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function getNode(llist, positionFromTail) {
+  // Write your code here
+}
